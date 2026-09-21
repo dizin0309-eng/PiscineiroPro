02c395868743f1375ba62b5a225dcb47bb453b69
Diego Ferreira <dizin0309@gmail.com>
2026-09-21 09:05:03 -0300

    Versao inicial do PiscineiroPro

---
+++ supabase-config.js
@@ -0,0 +1,5 @@
+﻿window.PISCINEIROPRO_CONFIG = {
+  url: "https://sqvixzxexvchzidsywdj.supabase.co",
+  publishableKey: "sb_publishable__zosqhnNDRfYbvM8QE6eZA_TBRJrDdP",
+  anonKey: "sb_publishable__zosqhnNDRfYbvM8QE6eZA_TBRJrDdP"
+};

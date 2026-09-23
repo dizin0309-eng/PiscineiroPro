e2295a116e932e41d0aff5aa292884b78800dec4
Diego Ferreira <dizin0309@gmail.com>
2026-09-23 15:23:15 -0300

    Atualiza cache do PWA

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='piscineiropro-fase1-v1';
+const CACHE='piscineiropro-fase1-v2';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./supabase-config.js'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
